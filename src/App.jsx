9e617103d1dfe903b8d02a2011da6a302f9fbff3
smallnumbers0 <jchoi53@mail.ccsf.edu>
2025-07-10 12:54:31 -0700

    remove more tempalte code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -14,5 +11,4 @@ function App() {
     </>
   )
 }
-
 export default App
