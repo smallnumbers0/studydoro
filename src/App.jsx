4d63f56328d33129592fb4ee93067b4379a53d19
smallnumbers0 <jchoi53@mail.ccsf.edu>
2025-07-12 13:13:08 -0700

    run prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import Timer from './components/Timer'
+import { useState } from "react";
+import "./App.css";
+import Timer from "./components/Timer";
 
-import Buttons from './components/Buttons'
+import Buttons from "./components/Buttons";
 
 function App() {
-
   return (
     <>
       <div>
         <h1>Doom Scroll Doro</h1>
       </div>
       <Timer />
-      <Buttons/>
+      <Buttons />
     </>
-  )
+  );
 }
-export default App
+export default App;
