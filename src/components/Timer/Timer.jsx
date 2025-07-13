b5aada9ccc407c429fbe02056978eb3c88b044a7
smallnumbers0 <jchoi53@mail.ccsf.edu>
2025-07-12 20:08:27 -0700

    restyle timer container and main title

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -3,9 +3,9 @@ import "./Timer.css";
 function Timer({ time, isWorkTime }) {
   return (
     <div className="timer-container">
-      <h1 className="timer-label">
+      <h2 className="timer-label">
         {isWorkTime ? "Work Time" : "Scroll Time"}
-      </h1>
+      </h2>
       <h2 className="timer">
         {String(time.mins).padStart(2, "0")}:{String(time.secs).padStart(2, "0")}
       </h2>
